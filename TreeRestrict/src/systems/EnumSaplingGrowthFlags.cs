9ffeff0c4489e4d82249513b45ff5e82c9f95aa2
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2025-10-22 14:16:30 -0500

    tried out using enums for flags

---
+++ TreeRestrict/src/systems/EnumSaplingGrowthFlags.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeRestrict.src.systems
+{
+    [Flags]
+    internal enum EnumSaplingGrowthFlags
+    {
+        Tempurature = 1,
+
+        Rainfall = 2,
+
+        Fertility = 4,
+
+        ForestDensity = 8,
+
+        Height = 0x10,
+
+        Custom = 32
+    }
+}
